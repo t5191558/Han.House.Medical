de8dac41416d12da58830939409e845abc454413
tianlei <tian519lei@163.com>
2021-10-01 11:03:32 +0800

    初始化项目

---
+++ FurionApi.Application/System/Dtos/Mapper.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+
+namespace FurionApi.Application
+{
+    public class Mapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+        }
+    }
+}
---
+++ FurionApi.EntityFramework.Core/DbContexts/DefaultDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Furion.DatabaseAccessor;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurionApi.EntityFramework.Core
+{
+    [AppDbContext("FurionApi", DbProvider.Sqlite)]
+    public class DefaultDbContext : AppDbContext<DefaultDbContext>
+    {
+        public DefaultDbContext(DbContextOptions<DefaultDbContext> options) : base(options)
+        {
+        }
+    }
+}
